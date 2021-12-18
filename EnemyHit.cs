396edc0ebdba65375e4787d029119db0be15f662
KimTaeyeon-08 <ssg9732@gmail.com>
2021-12-19 02:19:51 +0900

    upload cs file

---
+++ EnemyHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour
+{
+    public ParticleSystem explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Bullet")
+        {
+            Destroy(gameObject);
+            Destroy(col.gameObject);
+            Instantiate(explosion, transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 3f;
+    public float rotatespeed = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0,- rotatespeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, rotatespeed * Time.deltaTime);
+        }
+    }
+}
